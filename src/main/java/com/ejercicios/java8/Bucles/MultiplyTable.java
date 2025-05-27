1b775f8ed0849972912126137a7e02681f8817ed
Santiagomadaw <santiagoma.daw@gmail.com>
2025-05-27 19:06:37 +0200

    Mejora vista en columnas

--- src/main/java/com/ejercicios/java8/Bucles/MultiplyTable.java
+++ src/main/java/com/ejercicios/java8/Bucles/MultiplyTable.java
@@ -1,14 +1,27 @@
 //Mostrar una tabla de multiplicar del 1 al 10.
-
+//todo añadir los espacios enecesarios para que las tablas queden alineadas por columnas
 package com.ejercicios.java8.Bucles;
 
 public class MultiplyTable {
     public static void main(String[] args) {
-    for (int firstParam = 1; firstParam <= 10; firstParam++) {
+    for (int firstParam = 0; firstParam <= 10; firstParam++) {
+        
         for (int secondParam = 1; secondParam <= 10; secondParam++) {
-            System.out.println(firstParam + " x " + secondParam + " = " + (firstParam * secondParam));
+            
+            if (firstParam == 0) {
+                System.out.print(" Tabla del " + secondParam+ "\t");
+                if (secondParam == 10) {
+                    System.out.print("\n");
+                }
+            }else{
+                System.out.print(" " + secondParam + " x " + firstParam + " = " + (firstParam * secondParam)+ "\t");
+                if (secondParam == 10) {
+                    System.out.print("\n");
+                }
+            }
         }
-    }
-
+       
+        }
+            
     }
 }
