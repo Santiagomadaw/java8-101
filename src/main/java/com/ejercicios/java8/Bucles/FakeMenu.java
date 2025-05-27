be2ebc5ea54c48eef6902a69d4073d817bedb099
Santiagomadaw <santiagoma.daw@gmail.com>
2025-05-28 00:27:11 +0200

    Arreglo control de input. control de errores

--- src/main/java/com/ejercicios/java8/Bucles/FakeMenu.java
+++ src/main/java/com/ejercicios/java8/Bucles/FakeMenu.java
@@ -1,7 +1,15 @@
 //Simula un menú interactivo con do-while que permita repetir acciones hasta que el usuario diga "salir".
 
 package com.ejercicios.java8.Bucles;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+/* no se si un enum es la mejor solucion
+ * pero creo que es una buena alternativa
+ * para simular la selección de opciones
+ */
+
 enum Option {
     EXIT,
     FILE,
@@ -9,32 +17,40 @@ enum Option {
     SELECTION,
     VIEW,
     TERMINAL,
-    
+
 }
 
 public class FakeMenu {
+
     public static void main(String[] args) {
-       
+
         Scanner scanner = new Scanner(System.in);
         int option = -1;
         do {
             System.out.println("Menu");
-            System.out.println("1."+ Option.FILE+"\n");
-            System.out.println("2."+ Option.EDIT+"\n");
-            System.out.println("3."+ Option.SELECTION+"\n");
-            System.out.println("4."+ Option.VIEW+"\n");	
-            System.out.println("5."+ Option.TERMINAL+"\n");
-            System.out.println("0."+ Option.EXIT+"\n");
+            System.out.println("1." + Option.FILE);
+            System.out.println("2." + Option.EDIT);
+            System.out.println("3." + Option.SELECTION);
+            System.out.println("4." + Option.VIEW);
+            System.out.println("5." + Option.TERMINAL);
+            System.out.println("0." + Option.EXIT);
             System.out.println("Enter an option: ");
-            option = scanner.nextInt();
-            System.out.println("You selected option " + Option.values()[option]);
-        } while (option != 0);
+            try {
 
-        if (option >= 0 && option <= 5) {
-            Option selected = Option.values()[option];
-            System.out.println("Elegiste: " + selected);
-        } else {
-            System.out.println("Por favor elige entre 0 y 5.");
-        }
+                option = scanner.nextInt();
+                if (option >= 0 && option <= 5) {
+                    Option selected = Option.values()[option];
+                    System.out.println("You choose: " + selected);
+                } else {
+                    System.out.println("Please select between 0 and 5.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input, only numbers are allowed");
+                scanner.next(); // limpia el buffer evita bucle infinito
+            }
+
+        } while (option != 0);
+        scanner.close(); // cierra el scanner para liberar recursos
+        return;
     }
 }
