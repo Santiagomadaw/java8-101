5f3c6f9e31e9f11d0ffa39817f18e33986ce7b48
Santiagomadaw <santiagoma.daw@gmail.com>
2025-05-28 01:30:13 +0200

    feat: creacion de matrices 3x3, primero vacios e incluimos valores en 2 bucles anidados y despues dando valores directamente en la declaración

---
+++ src/main/java/com/ejercicios/java8/TablesNArrays/NineMatrix.java
@@ -0,0 +1,28 @@
+//Crear una matriz de 3x3 y llenar con la secuencia del 1 al 9.
+
+package com.ejercicios.java8.TablesNArrays;
+
+public class NineMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];// en un array de arrays tambien se se debe declarar el tamaño.
+        int count = 1;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = count;
+                System.out.print(matrix[i][j] + "\t");
+                count++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int[][] matrix2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };// se pueden declarar los valores de los arrays en
+                                                                    // la misma linea
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix2[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
