62ccb475b3c3bb55b6d0c68e55f55167cd48011c
Santiagomadaw <santiagoma.daw@gmail.com>
2025-05-28 01:18:47 +0200

    Style:  Doy estilo a la muestra de la suma

--- src/main/java/com/ejercicios/java8/TablesNArrays/SumArray.java
+++ src/main/java/com/ejercicios/java8/TablesNArrays/SumArray.java
@@ -6,14 +6,15 @@ import java.util.Random;
 
 public class SumArray {
     public static void main(String[] args) {
-        int[] array = new int[10]; // es necesario declarar el tamañao del array
+        int[] array = new int[10];// es necesario declarar el tamañao del array
         Random random = new Random();
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(100);
             sum += array[i];
-            System.out.println(array[i] + " + ");
+            System.out.println(array[i] + "\t+");
         }
-        System.out.println("Suma de los elementos del array: " + sum);
+        System.out.println("-----------");
+        System.out.println(sum);
     }
 }
