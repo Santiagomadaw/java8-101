fa5a5b0e754fe83a4123f7132b430f5f971ed9ea
Santiagomadaw <santiagomadaw@gmail.com>
2025-05-31 11:00:32 +0200

    Ampliacion del uso de Enum, incluyendo atributos y metodos. Uso de HashMap

--- src/main/java/com/ejercicios/java8/Bucles/FakeMenu.java
+++ src/main/java/com/ejercicios/java8/Bucles/FakeMenu.java
@@ -2,7 +2,9 @@
 
 package com.ejercicios.java8.Bucles;
 
+import java.util.HashMap;
 import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.Scanner;
 
 /* no se si un enum es la mejor solucion
@@ -11,43 +13,74 @@ import java.util.Scanner;
  */
 
 enum Option {
-    EXIT,
-    FILE,
-    EDIT,
-    SELECTION,
-    VIEW,
-    TERMINAL,
+    EXIT(0,"Salir"),
+    FILE(1, "Archivo"),
+    EDIT(2,"Editar"),
+    SELECTION(3,"Seleccion"),
+    VIEW(4,"Vista"),
+    TERMINAL(5,"Terminal");
+    private final int numOption;
+    private final String description;
 
+    private static final Map<Integer, Option> OPTION_MAP = new HashMap<>();
+    static {
+        for (Option option : values()) {
+            OPTION_MAP.put(option.numOption, option);
+        }
+    }
+
+
+    Option (int numOption, String description){
+        this.description= description;
+        this.numOption=numOption;
+
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getNumOption() {
+        return numOption;
+    }
+    public static String executeOption(int o) {
+        Option option = OPTION_MAP.get(o);
+        if (option==null){return "Opcion no valida";}
+        switch (option) {
+            case EXIT: return "Saliendo del programa...";
+            case FILE: return "Abriendo menú de archivo...";
+            case EDIT: return "Entrando al modo edición...";
+            case SELECTION: return "Modo selección activado.";
+            case VIEW: return "Mostrando opciones de vista.";
+            case TERMINAL: return "Ejecutando terminal integrada.";
+            default: return "Opción no reconocida.";
+        }
+    }
 }
 
 public class FakeMenu {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        
         Scanner scanner = new Scanner(System.in);
+        
         int option = -1;
         do {
-            System.out.println("Menu");
-            System.out.println("1." + Option.FILE);
-            System.out.println("2." + Option.EDIT);
-            System.out.println("3." + Option.SELECTION);
-            System.out.println("4." + Option.VIEW);
-            System.out.println("5." + Option.TERMINAL);
-            System.out.println("0." + Option.EXIT);
-            System.out.println("Enter an option: ");
-            try {
+            System.out.print("\033[H\033[2J");
+    System.out.flush();
+            for(Option o : Option.values()){
+                System.out.println(o.getNumOption()+" "+o.getDescription());
+    
+            }
+            System.out.print("Enter an option: ");
+             try {
 
                 option = scanner.nextInt();
-                if (option >= 0 && option <= 5) {
-                    Option selected = Option.values()[option];
-                    System.out.println("You choose: " + selected);
-                } else {
-                    System.out.println("Please select between 0 and 5.");
-                }
+                System.out.println(Option.executeOption(option));
+                Thread.sleep(2000); 
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input, only numbers are allowed");
                 scanner.next(); // limpia el buffer evita bucle infinito
             }
+           
 
         } while (option != 0);
         scanner.close(); // cierra el scanner para liberar recursos
